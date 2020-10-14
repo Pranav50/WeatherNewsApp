c0e5d072375cdde6b4461d02a7236016614c63da
Pranav Pawar <pranavpwr815@gmail.com>
2020-10-14 16:20:47 +0530

    Updates

---
+++ precache-manifest.4dffcb91d06bc917d5cfacece5c1bf38.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8fb68e883400347e4b9c000dae43a8",
+    "url": "/WeatherNewsApp/index.html"
+  },
+  {
+    "revision": "e1acfa148c9cb69c2b36",
+    "url": "/WeatherNewsApp/static/css/2.f4d32d89.chunk.css"
+  },
+  {
+    "revision": "ce4b5a66b7392dbdc389",
+    "url": "/WeatherNewsApp/static/css/main.7bb31e96.chunk.css"
+  },
+  {
+    "revision": "e1acfa148c9cb69c2b36",
+    "url": "/WeatherNewsApp/static/js/2.ce8373e9.chunk.js"
+  },
+  {
+    "revision": "eae039b9dbeb27c35b2aac72664ea72b",
+    "url": "/WeatherNewsApp/static/js/2.ce8373e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4b5a66b7392dbdc389",
+    "url": "/WeatherNewsApp/static/js/main.97986aae.chunk.js"
+  },
+  {
+    "revision": "b8915fead206f41c913e",
+    "url": "/WeatherNewsApp/static/js/runtime-main.ac7b8df3.js"
+  },
+  {
+    "revision": "7144f2e5e20bce718e35dc36bfc087d7",
+    "url": "/WeatherNewsApp/static/media/afternoon-sun.7144f2e5.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/WeatherNewsApp/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/WeatherNewsApp/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "55411c341229354f81c769d0c97c1492",
+    "url": "/WeatherNewsApp/static/media/evening-sun.55411c34.png"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/WeatherNewsApp/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/WeatherNewsApp/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/WeatherNewsApp/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/WeatherNewsApp/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/WeatherNewsApp/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/WeatherNewsApp/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/WeatherNewsApp/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/WeatherNewsApp/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/WeatherNewsApp/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/WeatherNewsApp/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/WeatherNewsApp/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/WeatherNewsApp/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/WeatherNewsApp/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/WeatherNewsApp/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/WeatherNewsApp/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "d0a5384ef91c4b8b24c0b35375faca89",
+    "url": "/WeatherNewsApp/static/media/moon-bats.d0a5384e.png"
+  },
+  {
+    "revision": "b6ccc98a4a924ece6905e111424eabe3",
+    "url": "/WeatherNewsApp/static/media/moon.b6ccc98a.png"
+  },
+  {
+    "revision": "c7a411ca6500e609a1425b600f1abf33",
+    "url": "/WeatherNewsApp/static/media/morning-sun.c7a411ca.png"
+  },
+  {
+    "revision": "52fea669711f643cbf5e30d2df955110",
+    "url": "/WeatherNewsApp/static/media/page-under-construction.52fea669.png"
+  },
+  {
+    "revision": "72174c72301a10fe33d9757662c705f9",
+    "url": "/WeatherNewsApp/static/media/weather2.72174c72.gif"
+  },
+  {
+    "revision": "f3fcd4a5b41ee71d9837508b6934a17f",
+    "url": "/WeatherNewsApp/static/media/weather_banner.f3fcd4a5.jpg"
+  }
+]);
