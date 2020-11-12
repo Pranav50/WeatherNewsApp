50904de829a6e3baf47e7d83341cbc806d1a1411
Pranav Pawar <pranavpwr815@gmail.com>
2020-11-12 15:16:58 +0530

    Update MadeWithLove.js
    Added code for Github Link

--- src/components/MadeWithLove.js
+++ src/components/MadeWithLove.js
@@ -12,7 +12,7 @@ const MadeWithLove = (props) => {
         <a style={{...(props.isDayMode ? { color: "#535c68" } : { color: "white"})}} 
         href="https://www.linkedin.com/in/pranav-pawar-661b9838/" target="_blank">
         Pranav Pawar</a>
-    </span>
+    </span> <span style={{...(props.isDayMode ? { color: "#535c68" } : { color: "white"})}}>|</span> {props.isDayMode ? <a target="_blank" href="https://github.com/Pranav50/WeatherNewsApp"><img style={{marginBottom:'0.7rem', height:'25px', width:'25px'}} src={github} /></a> : <a href="https://github.com/Pranav50/WeatherNewsApp" target="_blank"><img style={{marginBottom:'0.7rem', height:'25px', width:'25px'}} src={githubWhite} /></a>}
         </div>
     )
 }
